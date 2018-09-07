adfa0ca4dabb13eaf36262ca1f3196e0c9813061
corbett.phil <corbett.phil@gmail.com>
2018-09-07 11:29:27 -0400

    Update the example to injest a single function

--- html/example.js
+++ html/example.js
@@ -1,22 +1,62 @@
-(function (win) {
+var alive = (function (){
+  var reporters = [];
+  
+  function ingest(code) {
+    var fun = Function('"use strict"; return ' + code)();
+    sendReport(fun);
+    return wrapFunction(fun);
+  }
+  
+  function wrapFunction(inFunc) {
+    var fMain = {
+      runs: [],
+      func: inFunc,
+      f: function () {
+        var input = arguments;
+        var output = fMain.func.apply(null, arguments);
+        fMain.runs.push({
+          input: input,
+          output: output
+        });
+        
+        sendReport(fMain);
+        return output;
+      }
+    };
+    return fMain.f;
+  }
+  
+  function registerReporter(func) {
+    reporters.push(func);
+  }
+  
+  function sendReport(report) {
+    var i = 0;
+    for(i = 0; i < reporters.length; i++) {
+      if(reporters[i]) {
+        reporters[i](report);
+      }
+    }
+  }
+  
+  return {
+    ingest: ingest,
+    registerReporter: registerReporter
+  };
+}());
+
+(function (win, alive) {
   var codeEditor = win.document.getElementById('code');
   var outputPanel = win.document.getElementById('output');
   var commitButton = win.document.getElementById('btnCommit');
   
   commitButton.onclick = function () {
-    codeEditor.innerHTML;
+    window.testFunc = alive.ingest(codeEditor.innerText);
   };
+  
+  alive.registerReporter(function (report) {
+    outputPanel.innerHTML = JSON.stringify(report);
+  });
   
   outputPanel.innerHTML = 'output test';
-  codeEditor.innerHTML = 'enter code here';
-}(window));
-
-var alive = (function (){
-  function ingest(code) {
-  	eval(code);  
-  }
-  
-  return {
-    ingest: ingest
-  };
-}());
+}(window, alive));
